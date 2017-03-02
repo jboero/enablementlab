c7e1c81609e145a0b6bfb3c8e09f2b82a41a80ef
Nicolas Corrarello <nicolas@corrarello.com>
2017-03-02 15:32:32 +0000

    Added database, example terraform.tfvars and ammended readme

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Clone this repo, and configure the following variables (probably on terraform.tf
 - awsaccountid
 - namespace: A unique ID for the lab
 - students: List of people assisting to the lab, will be used for usernames on the instances for login, and for the storage path in Consul for each Vault cluster (Example: ["ncorrare","bgreen"])
+
+
 Drop the vault.zip enterprise Binary on modules/students/files/, it's copied, installed and set up automatically.
 
 Once it's done, the environment variables are set up automatically for each user, just try a vault init and go from there.
--- main/main.tf
+++ main/main.tf
@@ -48,6 +48,26 @@ module "directory" {
                      ]
 }
 
+module "database" {
+  source = "../modules/node"
+  
+  namespace        = "${var.namespace}"
+  servername       = "database"
+  aws_amis         = "${var.aws_amis}"
+  aws_region       = "${var.aws_region}"
+  sshkey           = "${var.sshkey}"
+  keypath          = "${var.keypath}"
+  subnet_id        = "${module.common.subnet_id}"
+  instance_type    = "t2.micro"
+  public_ip        = "true"
+  sec_group        = "${module.common.sec_group_generic_id}"
+  provision_script = [ "/usr/bin/sudo /sbin/setenforce 0",
+                       "/bin/curl https://raw.githubusercontent.com/ncorrare/terraform-examples/master/provision.sh | /usr/bin/sudo /bin/bash",
+                       "/usr/bin/sudo FACTER_consulserver=${module.consul.public_hostname} /opt/puppetlabs/bin/puppet apply --environment enablementlab -e 'include profile::database'"
+                     ]
+}
+
+
 module "students" {
   source = "../modules/students"
   
@@ -67,4 +87,5 @@ module "students" {
 
 output "student-servers" { value = "${module.students.vault-servers}" }
 output "directory-server" { value = "ssh ec2-user@${module.directory.public_hostname}" }
+output "database-server" { value = "ssh ec2-user@${module.database.public_hostname}" }
 output "consul-server" { value = "ssh ec2-user@${module.consul.public_hostname} -L 8500:localhost:8500" }
